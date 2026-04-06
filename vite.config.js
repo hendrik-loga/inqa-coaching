138f54e34d8cd2a93237686035796cc5da4bc18c
hendrik-loga <134273648+hendrik-loga@users.noreply.github.com>
2026-04-06 20:35:36 +0200

    fix: vite config for excalidraw

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@excalidraw/excalidraw'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/@excalidraw\/excalidraw/, /node_modules/],
+    },
+  },
 })
